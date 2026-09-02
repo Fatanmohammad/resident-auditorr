8008e86946766e012856a90bc55dbd302cce989d
awulul <anisaaualia@gmail.com>
2026-09-03 03:45:38 +0800

    feat: define fillable properties and relations in offsite model

--- app/Models/Offsite/AuditLog.php
+++ app/Models/Offsite/AuditLog.php
@@ -2,9 +2,28 @@
 
 namespace App\Models\Offsite;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class AuditLog extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 
+        'kode_unit', 
+        'jenis_file', 
+        'nama_file', 
+        'status', 
+        'total_low', 
+        'total_moderate', 
+        'total_high', 
+        'pesan_error'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/Offsite/MasterKeyword.php
+++ app/Models/Offsite/MasterKeyword.php
@@ -2,9 +2,15 @@
 
 namespace App\Models\Offsite;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class MasterKeyword extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'kategori_risiko',
+        'keyword'
+    ];
 }
